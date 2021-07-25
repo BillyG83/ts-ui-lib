709549d7efb4138a51154e4b2134af1997ea495e
BillyG83 <bgilmore83@hotmail.co.uk>
2021-07-25 15:50:56 +0200

    feat(app): initial setup files

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>
+          Hello Typescript!
+        </h1>
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
