d63f8a7a073bf9a8514e6c3d993c4c75ebf9a437
BillyG83 <bgilmore83@hotmail.co.uk>
2021-07-27 19:42:30 +0200

    feat(form): using Input for dynamic state hook

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,33 +1,52 @@
 import React, { useState } from 'react'
 import { Button } from '../button/Button'
+import { Input } from '../input/Input'
 import { UserInterface } from '../interface'
 
-export const Form = () => {
-    const [ user, setUser ] = useState<UserInterface | null>(null)
+const initialData: UserInterface = {
+    name: '',
+    age: 0,
+    country: '',
+    address: {
+        street: '',
+        number: 0,
+        zip: '',
+    },
+    admin: false,
+}
 
-    const fetchUser = () => setUser({
-        name: 'Bill',
-        age: 37,
-        country: 'Spain',
-        address: {
-            street: 'Calle Tornado',
-            number: 2,
-            zip: '123456',
-        },
-        admin: false,
-    })
+export const Form = () => {
+    const [ user, setUser ] = useState<UserInterface>(initialData)
 
     const handleClick = (data: string) => {
-        fetchUser()
         console.log('button ID: ' + data);
+    }
+
+    const inputValueChanged = (inputName: string, inputValue: string) => {
+        setUser((prevState) => ({
+            ...prevState,
+            [inputName] : inputValue
+        }))
     }
 
     return (
         <div>
+            <Input 
+                inputId="name-input"
+                inputName="name"
+                inputPlaceholder="name"
+                valueChanged={inputValueChanged}
+                inputType="text"
+            />
+            <Input 
+                inputId="age-input"
+                inputName="age"
+                inputPlaceholder="age"
+                valueChanged={inputValueChanged}
+                inputType="number"
+            />
+
             <Button buttonType="submit" buttonId="data-button" text="get data" clickHandle={handleClick} />
-            {
-                user && <p>Welcome {user?.name}</p>
-            }
         </div>
     ) 
 } 
