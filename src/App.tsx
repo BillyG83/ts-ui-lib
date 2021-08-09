0cdb931ef203514f409f61da37a60bd57ae53e80
BillyG83 <bgilmore83@hotmail.co.uk>
2021-08-09 19:19:16 +0200

    chore(App): cleaning up

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,10 @@
 import React from 'react';
 import './App.css';
-
-import Hero from './components/hero/Hero';
 import Form from './components/form/Form';
-import Button from './components/button/Button';
-import Switch from './components/switch/Switch'
-
-const imgUrl = 'https://www.wallpaperimageshd.com/uploads/2019/10/scenery-background-07.jpg'
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Hero 
-        title="Hey Typescript" 
-        subTitle="fun"
-        backgroundColor="rgba(0,0,0,0.5)"
-        backgroundImage={imgUrl}
-        maxHeight="90vh"
-        filterOpacity={10}
-      >
-        <Button 
-          buttonId="hero-button"
-          clickHandle={(data) => { console.log(data) }}
-          text="CTA"
-          theme="dark"
-        />
-        <Switch 
-          switchID="hero-switch"
-          switchLabel="Switch!"
-          valueChanged={(id, isOn) => { console.log(id, isOn) }}
-        />
-        <Switch 
-          switchID="nintendo-switch"
-          switchLabel="Switch!"
-          valueChanged={(id, isOn) => { console.log(id, isOn) }}
-        />
-      </Hero>
       <div className="wrap">
         <Form />
       </div>
