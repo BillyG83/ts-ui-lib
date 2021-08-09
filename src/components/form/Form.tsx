ee80c3c58535b52363bd352e3d2b7f768654e79b
BillyG83 <bgilmore83@hotmail.co.uk>
2021-08-09 19:18:57 +0200

    chore(Form): importing Switch and Range to test integration

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import Button from '../button/Button'
 import RadioGroup from '../radioGroup/RadioGroup'
 import Input from '../input/Input'
+import Switch from '../switch/Switch'
+import Range from '../range/Range'
+
 import { UserInterface } from '../interface'
 
 const initialData: UserInterface = {
@@ -42,60 +45,105 @@ const Form: React.FC = () => {
     }
 
     return (
-        <form>
-            <Input 
-                inputId="name-input"
-                inputLabel="enter your full name"
-                inputName="name"
-                inputPlaceholder="name"
-                inputType="text"
-                valueChanged={inputValueChanged}
-            />
-            <Input 
-                inputId="password-input"
-                inputLabel="add a password"
-                inputName="password"
-                inputPlaceholder="password"
-                inputType="password"
-                valueChanged={inputValueChanged}
-            />
-            <Input 
-                inputId="age-input"
-                inputLabel="enter your age"
-                inputName="age"
-                inputPlaceholder="age"
-                inputType="number"
-                valueChanged={inputValueChanged}
-            />
+        <form onSubmit={(e) => e.preventDefault()}>
+            <div className="spacer">
+                <Range
+                    rangeId="rangers"
+                    rangeMax={100}
+                    rangeMin={0}
+                    rangeValue={50}
+                ></Range>
+            </div>
 
-            <Input 
-                inputId="checkbox-input"
-                inputLabel="tick to get admin"
-                inputName="admin"
-                inputType="checkbox"
-                valueChanged={inputValueChanged}
-            />
-
-            <RadioGroup id="sex">
+            <div className="spacer">
                 <Input 
-                    inputId="radio-male"
-                    inputLabel="Male"
-                    inputName="sex"
-                    inputValue="male"
-                    inputType="radio"
+                    inputId="name-input"
+                    inputLabel="enter your full name"
+                    inputName="name"
+                    inputPlaceholder="name"
+                    inputType="text"
                     valueChanged={inputValueChanged}
                 />
+            </div>
+            
+            <div className="spacer">
+                <Switch
+                    switchLabel="do you like chips?"
+                    switchID="chip-fan"
+                    valueChanged={inputValueChanged}
+                    switchName="chips"
+                    switchOnText="on"
+                    switchOffText="off"
+                ></Switch>
+            </div>
+
+            <div className="spacer">
+                <Switch
+                    switchLabel="do you like dips?"
+                    switchID="dip-fan"
+                    valueChanged={inputValueChanged}
+                    switchName="dips"
+                    switchOnText="on"
+                    switchOffText="off"
+                ></Switch>
+            </div>
+
+            <div className="spacer">
                 <Input 
-                    inputId="radio-female"
-                    inputLabel="Female"
-                    inputName="sex"
-                    inputValue="female"
-                    inputType="radio"
+                    inputId="password-input"
+                    inputLabel="add a password"
+                    inputName="password"
+                    inputPlaceholder="password"
+                    inputType="password"
                     valueChanged={inputValueChanged}
                 />
-            </RadioGroup>
+            </div>
 
-            <Button buttonType="submit" buttonId="data-button" text="get data" clickHandle={handleClick} />
+            <div className="spacer">
+                <Input 
+                    inputId="age-input"
+                    inputLabel="enter your age"
+                    inputName="age"
+                    inputPlaceholder="age"
+                    inputType="number"
+                    valueChanged={inputValueChanged}
+                />
+            </div>
+
+            <div className="spacer">
+                <Input 
+                    inputId="checkbox-input"
+                    inputLabel="tick to get admin"
+                    inputName="admin"
+                    inputType="checkbox"
+                    valueChanged={inputValueChanged}
+                />
+            </div>
+
+            <div className="spacer">
+                <RadioGroup id="sex">
+                    <Input 
+                        inputId="radio-male"
+                        inputLabel="Male"
+                        inputName="sex"
+                        inputValue="male"
+                        inputType="radio"
+                        valueChanged={inputValueChanged}
+                    />
+                    <Input 
+                        inputId="radio-female"
+                        inputLabel="Female"
+                        inputName="sex"
+                        inputValue="female"
+                        inputType="radio"
+                        valueChanged={inputValueChanged}
+                    />
+                </RadioGroup>
+            </div>
+
+            <div className="spacer">
+                <Button buttonType="submit" buttonId="data-button" text="get data" clickHandle={handleClick} />
+            </div>
         </form>
     ) 
 }
