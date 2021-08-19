1293aa09b25226a39bc868e68975dc1252788771
BillyG83 <bgilmore83@hotmail.co.uk>
2021-08-19 16:49:57 +0200

    chore(App): Importing Hero and Button for some testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
-import Form from './components/form/Form';
+import React from 'react'
+import './App.css'
+import Form from './components/form/Form'
+import Hero from './components/hero/Hero'
+import Button from './components/button/Button'
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Hero
+        title="Hero Wins!"
+        backgroundImage="https://www.fillmurray.com/2000/2000"
+        filterColor="rgb(17, 17, 17)"
+        filterOpacity={0.25}
+      >
+        <Button 
+          buttonId="hero-button" 
+          clickHandle={(id)=>(console.log('button ID:' + id))} 
+        />
+      </Hero>
+      
       <div className="wrap">
         <Form />
       </div>
